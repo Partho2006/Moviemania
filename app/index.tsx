9a2580f8529aa3966e7c283ae23cb779e22cf2c9
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-11-09 19:07:34 +0530

    1st

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-dark-200">
+        Welcome to Nativewind!
+      </Text>
+
+    </View>
+  );
+}
---
+++ app/movie/[id].tsx
@@ -0,0 +1,17 @@
+import { useLocalSearchParams } from 'expo-router';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const Details = () => {
+    const { id } = useLocalSearchParams();
+
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Details;
